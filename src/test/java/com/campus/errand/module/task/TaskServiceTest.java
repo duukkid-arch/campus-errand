8b024d45a7f0b00f4227d35a76f342b9e9351dc1
duukkid <duukkid@gmail.com>
2026-09-07 19:11:19 +0800

    test: add spring boot context test

---
+++ src/test/java/com/campus/errand/module/task/TaskServiceTest.java
@@ -0,0 +1,16 @@
+package com.campus.errand.module.task;
+
+import com.campus.errand.ErrandApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ErrandApplication.class)
+public class TaskServiceTest {
+
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
